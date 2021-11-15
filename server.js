56e1bb9ad94f99b2906df2571eb2fd18ecbf9261
Sherry Zheng <sherryzheng2018@gmail.com>
2021-11-14 22:08:49 -0800

    frontend integrated

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.use(express.static("public"))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database!'))
+
+app.use(express.json())
+
+
+const appRoute = require('./routes/frontendRoutes/app-route')
+app.use('/', appRoute)
+
+app.listen(3000, () => console.log('Server Started!'))
+
